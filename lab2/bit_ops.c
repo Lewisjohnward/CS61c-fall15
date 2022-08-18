07064bd320f4982c704bf71c1e1d07051e4e8831
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-08-18 16:03:40 +0100

    exercise 1 lab 2 done

--- lab2/bit_ops.c
+++ lab2/bit_ops.c
@@ -8,7 +8,13 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    
+    //1<<n is the position of the queried bit
+    //(1<<b)&x and bitwise operator sets all bits other than queried bit to 0
+    //>>n shifts bit back to position 0 and hence zero or one!
+
+
+    return ((1<<n)&x)>>n;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
@@ -16,12 +22,16 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+
+    //NEED TO DO
+    
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    *x = (1<<n)^*x;
 }
 
 
